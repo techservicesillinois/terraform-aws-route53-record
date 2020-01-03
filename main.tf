d270f817bad221da934891e93856f99529cb8375
David D. Riddle <ddriddle@illinois.edu>
2020-01-03 13:40:40 -0600

    Terraform 0.12 migration

--- main.tf
+++ main.tf
@@ -1,10 +1,10 @@
 resource "aws_route53_record" "default" {
-  zone_id = "${data.aws_route53_zone.selected.zone_id}"
-  name    = "${var.hostname}"
-  type    = "${var.type}"
-  ttl     = "${var.ttl}"
+  zone_id = data.aws_route53_zone.selected.zone_id
+  name    = var.hostname
+  type    = var.type
+  ttl     = var.ttl
 
-  health_check_id = "${var.health_check_id}"
+  health_check_id = var.health_check_id
 
-  records = ["${var.records}"]
+  records = var.records
 }
